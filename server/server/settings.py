b408ec82b4a1fe5f111904be6eeb8ca894ac4d8a
fidelquedet <fidelquedet10@gmail.com>
2023-07-17 09:46:54 +0000

    AWS S3 configuration

--- server/server/settings.py
+++ server/server/settings.py
@@ -128,3 +128,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AWS_ACCESS_KEY_ID = "AKIAQGXZWIGQSUNHCJ5L"
 AWS_SECRET_ACCESS_KEY = "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"
+
+# AWS S3 configuration
+
+AWS_STORAGE_BUCKET_NAME = "flx-photos"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
