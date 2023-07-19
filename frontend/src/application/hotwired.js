a582e0388efd86a7b26a44d959c1708130c3654a
fidelquedet <fidelquedet10@gmail.com>
2023-07-19 09:56:00 +0000

    Responsive Layout

--- frontend/src/application/hotwired.js
+++ frontend/src/application/hotwired.js
@@ -3,8 +3,10 @@ import { Application } from "@hotwired/stimulus";
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 import Lightbox from "stimulus-lightbox";
 import 'lightgallery/css/lightgallery.css';
+import { Slideover } from "tailwindcss-stimulus-components";
 
 window.Stimulus = Application.start();
 const context = require.context("../controllers", true, /\.js$/);
 window.Stimulus.load(definitionsFromContext(context));
 window.Stimulus.register('lightbox', Lightbox);
+window.Stimulus.register('slideover', Slideover);
